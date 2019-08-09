bd64bf88ffd1a8fc3ec5b41227296af9dff6291a
zhangyingying09 <516194010@qq.com>
2019-08-09 00:53:16 +0000

    update

--- resources/views/index/public/foot.blade.php
+++ resources/views/index/public/foot.blade.php
@@ -9,7 +9,7 @@
             </div>
         </div>
         <div class="fmenu">
-            <p><a href="/page/page">关于我们</a> | <a href="/page/page">联系我们</a> | <a href="#">优秀讲师</a> | <a href="#">帮助中心</a> | <a href="#">意见反馈</a> | <a href="#">加入我们</a></p>
+            <p><a href="/page/page">关于我们</a> | <a href="/page/pagecontact">联系我们</a> | <a href="#">优秀讲师</a> | <a href="#">帮助中心</a> | <a href="#">意见反馈</a> | <a href="#">加入我们</a></p>
         </div>
         <div class="copyright">
             <div><a href="/">谋刻网</a>所有&nbsp;晋ICP备12006957号-9</div>
